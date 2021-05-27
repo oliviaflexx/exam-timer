bd264fddf2851866802484366beceeddd3993e13
Olivia <oliviaflexx@gmail.com>
2021-05-27 12:07:39 -0400

    heroku second try

--- application.py
+++ application.py
@@ -1,7 +1,6 @@
 import os
 
 from flask import Flask, flash, redirect, render_template, request, session
-from flask_session import Session
 from tempfile import mkdtemp
 
 # Configure application
@@ -18,12 +17,6 @@ def after_request(response):
     response.headers["Expires"] = 0
     response.headers["Pragma"] = "no-cache"
     return response
-
-# Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
